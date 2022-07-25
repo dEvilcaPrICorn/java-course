c7d07f00a8844c59a73236bac681b418b8627bc0
Gentle.Devil <109566679+dEvilcaPrICorn@users.noreply.github.com>
2022-07-26 02:52:54 +0400

    update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -13,11 +13,25 @@ public class custom_order {
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
 	  
+Scanner keyboard = new Scanner (System.in); 
 
+String firstName; // User's first name
+String itemOrder; // Item ordered
+String frostingType; // Frosting ordered
+String fillingType; // Filling ordered
+String toppings; // Toppings ordered
+String input;  // User input
+
+double cost = 15.00; // Cost of cake and cupcakes
+final double TAX_RATE = .08;  // Sales tax rate
+double tax; // Amount of tax
 	 
     // Introduce shop and prompt user to input first name
       
+// Introduce shop and prompt user to input first name
 
+System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+System.out.println("We make custom cakes with our secret cake batter!");
 	  
 	  
     // TEST CODE
