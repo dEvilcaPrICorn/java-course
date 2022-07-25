967d7ea46b6a34dc124515a03555c6bde80581e7
Gentle.Devil <109566679+dEvilcaPrICorn@users.noreply.github.com>
2022-07-26 03:00:36 +0400

    update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -38,20 +38,34 @@ System.out.println("We make custom cakes with our secret cake batter!");
     
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
    
-	  
+	  System.out.print("What is your first name? ");
+firstName = keyboard.nextLine();
+
+System.out.print(firstName + ", please see our MENU below: ");
+System.out.print("\n"); // skips a line	  
       
     // TEST CODE     
     
     // STEP 4 DISPLAY MENU
 
-	
+	System.out.println("_______________________________________________");      
+System.out.println("        MENU         QUANTITY    BASE COST  ");
+System.out.println("_______________________________________________"); 
+System.out.println("        Cake                     1            $15     ");
+System.out.println("   Set of Cupcakes       6            $15     ");
+System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
+System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
+System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
+System.out.println("_______________________________________________");
       
 
     // TEST CODE     
     
     // STEP 5 PROMPT USER TO ORDER
 
-  
+  System.out.println("Do you want CUPCAKES or a CAKE?");
+itemOrder = keyboard.nextLine();
 	  
 	  
 	  
@@ -60,7 +74,9 @@ System.out.println("We make custom cakes with our secret cake batter!");
       
     // STEP 6 PROMPT USER TO CHOOSE FROSTING
 
-   
+   System.out.println("What type of FROSTING do you want? ");
+System.out.println("Vanilla, Chocolate, Strawberry or Coco");
+frostingType = keyboard.nextLine();
 	 
 	 
 	 
@@ -69,7 +85,9 @@ System.out.println("We make custom cakes with our secret cake batter!");
       
     // STEP 7 PROMPT USER TO CHOOSE FILLING
 
-
+System.out.println("What type of FILLING do you want? ");
+System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
+fillingType = keyboard.nextLine();
 	  
 	  
       
@@ -77,7 +95,9 @@ System.out.println("We make custom cakes with our secret cake batter!");
       
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
 
-
+System.out.println("What type of TOPPINGS do you want? ");
+System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+toppings = keyboard.nextLine();
 	
       
       
