e7894b894816eae90acdde48a250275115e85881
Gentle.Devil <109566679+dEvilcaPrICorn@users.noreply.github.com>
2022-07-26 03:23:22 +0400

    update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -125,5 +125,5 @@ System.out.println("_________________________________________");.
 tax = cost * TAX_RATE;
 System.out.printf("The tax is: $%.2f\n", tax);
 System.out.printf("The total due is: $%.2f\n",(tax + cost));
-    }   
-}
+    }   int totalCost
+}String addOnList
